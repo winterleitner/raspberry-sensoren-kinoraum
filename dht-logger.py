bc4eb9358a0af80427e05df2aa89f68ff35248e3
Emil Winterleitner <emil.winterleitner@student.tugraz.at>
2021-04-26 23:08:14 +0200

    working version of dht logger

--- dht-logger.py
+++ dht-logger.py
@@ -5,31 +5,7 @@ import board
 import adafruit_dht
 import mariadb
 import sys
-
-# Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT22(board.D4)
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-dhtDevice = adafruit_dht.DHT22(board.D2, use_pulseio=False)
-
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="root",
-        password="password",
-        host="192.168.5.5",
-        port=3306,
-        database="keller_sensor"
-
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-# Get Cursor
-cur = conn.cursor()
+import constant
 
 
 def log_db(temp, hum):
@@ -42,27 +18,56 @@ def log_db(temp, hum):
         print(f"Mariadb Error: {e}")
 
 
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+dhtDevice = adafruit_dht.DHT22(board.D2, use_pulseio=False)
+
+# Connect to MariaDB Platform
+connected = False
+tries = 0
+while connected is False and tries < 10:
+    try:
+        conn = mariadb.connect(
+            user="root",
+            password=constant.PWD,
+            host="192.168.5.5",
+            port=3306,
+            database="keller_sensor"
+
+        )
+        connected = True
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        tries += 1
+        time.sleep(2.0)
+
+if tries == 10:
+    sys.exit(-1)
+
+# Get Cursor
+cur = conn.cursor()
+
 while True:
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
+        # temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
         log_db(temperature_c, humidity)
-        #print(
-        #    "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-        #        temperature_f, temperature_c, humidity
-        #    )
-        #)
+        # print(
+        #     "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+        #         temperature_f, temperature_c, humidity
+        #     )
+        # )
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
-        #print("Error " + error.args[0])
-        time.sleep(2.0)
+        # print("Error " + error.args[0])
+        time.sleep(1.0)
         continue
     except Exception as error:
         dhtDevice.exit()
         raise error
 
-    time.sleep(3.0)
-
+    time.sleep(60.0)
