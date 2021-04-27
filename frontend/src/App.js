f65787076817706ab2d507769d53f12afbb9a2ad
Felix Winterleitner <felix.winterleitner@gmail.com>
2021-04-27 22:20:02 +0200

    added react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect} from "react";
+import {ThChart} from "./charts/ThChart";
+import {Table, Button} from "reactstrap";
+import DatePicker from "react-datepicker";
+
+const App = props => {
+    const [logs, setLogs] = useState([])
+    const [start, setStart] = useState(new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0, 0))
+    const [end, setEnd] = useState(new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59, 0))
+    const [show, setShow] = useState(100)
+
+    const filteredData = () => logs.filter(l => l.zeit > start && l.zeit < end)
+
+    useEffect(() => {
+        load()
+        window.setTimeout(load, 60000)
+    }, [])
+
+    const load = async () => {
+        const resp = await fetch("http://192.168.4.65/data.php")
+        const res = await resp.json()
+        const parsed = res.map(r => {
+                return {...r, zeit: new Date(r.zeit)}
+            })
+        setLogs(parsed)
+    }
+
+    const dtOpt = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+
+    return (
+        <div className="App">
+            <h1>Sensordaten Kinoraum</h1>
+            von
+            <div className="mb-2">
+                <DatePicker selected={start} onChange={date => setStart(date)} showTimeSelect dateFormat="Pp"
+                            className="form-control"/>
+            </div>
+            bis
+            <div>
+                <DatePicker selected={end} onChange={date => setEnd(date)} showTimeSelect dateFormat="Pp"
+                            className="form-control"/>
+            </div>
+            <ThChart data={logs.filter(l => l.zeit > start && l.zeit < end)}/>
+
+            <Table border="1" align="center" className="table table-dark table-striped">
+                <thead>
+                <tr>
+                    <th>Timestamp</th>
+                    <th>Temperature in Celcius</th>
+                    <th>Relative Humidity</th>
+                </tr>
+                </thead>
+                <tbody>
+                {filteredData().reverse().slice(0, show).map(l =>
+                    <tr>
+                        <td>{l.zeit.toLocaleTimeString("de-DE", dtOpt)}</td>
+                        <td>{l.temperatur} °C</td>
+                        <td>{l.luftfeuchtigkeit} %</td>
+                    </tr>
+                )}
+                </tbody>
+            </Table>
+            {show < logs.length && <Button color="success" onClick={() => setShow(prev => (prev + 100) < logs.length ? prev + 100 : logs.length)}>Mehr anzeigen</Button>}
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/charts/ThChart.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Line } from 'react-chartjs-2';
+
+const Chart = props => {
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                type: 'linear',
+                display: true,
+                position: 'left',
+            },
+            y1: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+
+                // grid line settings
+                grid: {
+                    drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
+                min: 0,
+                max: 30
+            }
+        },
+    };
+    const dtOpt = {month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'};
+
+    const data = {
+        labels: props.data.map(t => t.zeit.toLocaleTimeString("de-DE", dtOpt)),
+        datasets: [
+            {
+                label: "Temperature",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(59, 89, 152, 0.75)",
+                borderColor: "rgba(59, 89, 152, 1)",
+                pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
+                pointHoverBorderColor: "rgba(59, 89, 152, 1)",
+                data: props.data.map(t => t.temperatur),
+                yAxisID: 'y1'
+            },
+            {
+                label: "Humidity",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(29, 202, 255, 0.75)",
+                borderColor: "rgba(29, 202, 255, 1)",
+                pointHoverBackgroundColor: "rgba(29, 202, 255, 1)",
+                pointHoverBorderColor: "rgba(29, 202, 255, 1)",
+                data: props.data.map(t => t.luftfeuchtigkeit),
+                yAxisID: 'y',
+            }
+        ]
+    };
+
+    return <div className="chart-wrapper"><Line data={data} options={options} /></div>
+
+
+}
+
+export const ThChart = React.memo(Chart);
